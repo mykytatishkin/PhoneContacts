e33cdac2ad9ef601b6d5790b1f76e2e09279b589
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-07-31 18:21:59 +0300

    Add project files.

---
+++ PhoneContacts/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PhoneContacts
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView = new DataGridView();
+            AddButton = new Button();
+            UpdateButton = new Button();
+            deleteButton = new Button();
+            RefreshButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(12, 12);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowTemplate.Height = 25;
+            dataGridView.Size = new Size(489, 426);
+            dataGridView.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            AddButton.Location = new Point(507, 12);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(75, 23);
+            AddButton.TabIndex = 1;
+            AddButton.Text = "Add";
+            AddButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateButton
+            // 
+            UpdateButton.Location = new Point(507, 41);
+            UpdateButton.Name = "UpdateButton";
+            UpdateButton.Size = new Size(75, 23);
+            UpdateButton.TabIndex = 2;
+            UpdateButton.Text = "Update";
+            UpdateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(507, 70);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(75, 23);
+            deleteButton.TabIndex = 3;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Location = new Point(507, 99);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(75, 23);
+            RefreshButton.TabIndex = 4;
+            RefreshButton.Text = "Refresh";
+            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.Click += RefreshButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(RefreshButton);
+            Controls.Add(deleteButton);
+            Controls.Add(UpdateButton);
+            Controls.Add(AddButton);
+            Controls.Add(dataGridView);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView;
+        private Button AddButton;
+        private Button UpdateButton;
+        private Button deleteButton;
+        private Button RefreshButton;
+    }
+}
---
+++ PhoneContacts/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PhoneContacts
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        public string connectionString = "Data Source = OTISHKIN-NAS\\DEVELOP_2019; Initial Catalog = PhoneContacts; User ID = db_admin; Password =Qwerty.1; TrustServerCertificate = True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                // Open the database connection before populating the DataGridView
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                DataTable data = GetDataFromTable();
+                dataGridView.DataSource = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Make sure to close the connection after use
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Re-open the database connection before refreshing the DataGridView data
+                if (connection != null && connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                        MessageBox.Show("Connection open");
+                    else if (connection.State == ConnectionState.Closed)
+                        MessageBox.Show("Connection closed");
+                    else
+                        MessageBox.Show("No connection");
+                    
+                }
+
+                // Call the method to refresh the DataGridView data
+                DataTable data = GetDataFromTable();
+                dataGridView.DataSource = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Make sure to close the connection after use
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private DataTable GetDataFromTable()
+        {
+            DataTable dataTable = new DataTable();
+            string query = "SELECT * FROM Contacts"; // Replace YourTableName with your actual table name
+
+            // Ensure the connection is open before executing the query
+            if (connection != null && connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(dataTable);
+
+            return dataTable;
+        }
+    }
+}
