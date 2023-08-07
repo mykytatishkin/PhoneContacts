2535758d4b171469b5d81c9782164eb7c34ff6c7
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-08-07 14:35:37 +0300

    dependencies clearing

--- PhoneContactDatabase/Stored Procedures/GetAllContacts.sql
+++ PhoneContactDatabase/Stored Procedures/GetAllContacts.sql
@@ -1,6 +1,7 @@
 ﻿create procedure dbo.GetAllContacts
 as
 begin
+    set nocount on;
     select ID, FullName, TelephoneNumber, DateOfBirth 
     from dbo.Contacts;
 end;
--- PhoneContacts/Form1.cs
+++ PhoneContacts/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Data;
 using System.IO;
